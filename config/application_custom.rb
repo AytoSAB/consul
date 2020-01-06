491a4ca1f8be639a447f823725ccf3826935d415
AytoSAB <informatica@sabenaheber.com>
2020-01-06 18:02:40 +0100

    Seleccionar idiomas disponibles

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+     config.i18n.default_locale = :es
+     config.i18n.available_locales = [:es, :val]
+     config.i18n.fallbacks = {'val' => 'es'}
   end
 end
