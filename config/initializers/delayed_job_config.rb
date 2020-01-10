3e2e048fc744b9a9dd25bb5b69b68769f374d713
AytoSAB <informatica@sabenaheber.com>
2020-01-10 23:22:59 +0100

    Prueba deshabilitar cola

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,3 +1,4 @@
+=begin
 if Rails.env.test? || Rails.env.development?
   Delayed::Worker.delay_jobs = false
 elsif Rails.application.secrets.delay_jobs.nil?
@@ -14,3 +15,6 @@ Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = "default"
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, "log", "delayed_job.log"))
+=end
+
+Delayed::Worker.delay_jobs = false
