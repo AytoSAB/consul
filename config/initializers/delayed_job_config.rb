2c276de49beeab15cca9064b616e52655aad2e76
AytoSAB <informatica@sabenaheber.com>
2020-01-11 19:32:17 +0100

    Deshacer prueba delayed_jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,4 +1,3 @@
-=begin
 if Rails.env.test? || Rails.env.development?
   Delayed::Worker.delay_jobs = false
 elsif Rails.application.secrets.delay_jobs.nil?
@@ -15,6 +14,3 @@ Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = "default"
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, "log", "delayed_job.log"))
-=end
-
-Delayed::Worker.delay_jobs = false
